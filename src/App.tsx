8a5158d6626f8eeac76fd3fd5fdaa00079900a12
Hariom Sharma <hariom0sharma@gmail.com>
2020-07-29 00:20:07 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Button } from './lib'
+
+function App() {
+    return (
+        <div style={{
+            padding: 64,
+            border: '1px solid #ccc',
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0
+        }}>
+            <Button/>
+        </div>
+    )
+}
+
+export default App
